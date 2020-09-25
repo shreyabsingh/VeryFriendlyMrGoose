156c6ef263f998a65c1215347522c5951832e79c
shreyabsingh <62453189+shreyabsingh@users.noreply.github.com>
2020-09-25 16:01:01 -0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,24 @@
-input.buttonA.onEvent(ButtonEvent.Click, function () {
-	
-})
-input.buttonB.onEvent(ButtonEvent.Click, function () {
-    keyboard.key("", KeyboardKeyEvent.Up)
-})
 forever(function () {
     if (input.rotation(Rotation.Pitch) > 8) {
+        makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.rotation(Rotation.Pitch))
         light.showRing(
         `red red green red red red red red red red`
         )
     }
     if (input.rotation(Rotation.Pitch) < -8) {
+        makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.rotation(Rotation.Pitch))
         light.showRing(
         `red red red red red red red green red red`
         )
     }
     if (input.rotation(Rotation.Roll) < 8) {
+        makerController.player1.setAnalog(ArcadeAnalogButton.DownUp, input.rotation(Rotation.Roll))
         light.showRing(
         `green red red red red red red red red green`
         )
     }
     if (input.rotation(Rotation.Roll) > -8) {
+        makerController.player1.setAnalog(ArcadeAnalogButton.DownUp, input.rotation(Rotation.Roll))
         light.showRing(
         `red red red red green green red red red red`
         )
