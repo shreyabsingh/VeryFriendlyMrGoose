08d518fb1fd3afb4edda6a1215a9bb8447186f91
Eddy Zhou <71026430+Edwardius@users.noreply.github.com>
2020-09-25 11:35:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+	
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+	
+})
+forever(function () {
+    if (input.rotation(Rotation.Pitch) > 8) {
+        light.showRing(
+        `red red green red red red red red red red`
+        )
+    }
+    if (input.rotation(Rotation.Pitch) < -8) {
+        light.showRing(
+        `red red red red red red red green red red`
+        )
+    }
+    if (input.rotation(Rotation.Roll) < 8) {
+        light.showRing(
+        `green red red red red red red red red green`
+        )
+    }
+    if (input.rotation(Rotation.Roll) > -8) {
+        light.showRing(
+        `red red red red green green red red red red`
+        )
+    }
+})
